f7cd90ec972ee3f2b4431ae7c0c8c690098074f2
SaimarMukhija <93813780+SaimarMukhija@users.noreply.github.com>
2022-12-26 12:32:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ rightWristY = 0;
 rightWristX = 0;
 scoreRightWrist = 0;
 scoreleftWrist = 0;
+song1_status = "";
+song2_status = "";
 function setup() {
     canvas = createCanvas(600 , 500 );
     canvas.center();
@@ -19,6 +21,20 @@ function setup() {
 
 function draw(){
  image(video,0, 0 , 600 ,500);
+
+  fill('#FF0000');
+ stroke("#FF0000");
+ song1_status = song1.isPlaying();
+ song2_status = song2.isPlaying();
+ if(scoreleftWrist > 0.2)
+ {
+   circle(leftWristX, leftWristY,20);
+   song1.stop();
+   if(song2_status == false ) {
+      song2.play();
+      document.getElementById("songname").innerHTML = "playing peter pan song "
+   }
+ }
 
 }
  function preload()
